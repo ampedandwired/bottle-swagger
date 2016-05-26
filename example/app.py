5fd03d59e22ad6f6f07cc6b58d21c9cdac21f83e
Charles Blaxland <charles.blaxland@gmail.com>
2016-05-26 13:01:23 +1000

    Add example bottle app

---
+++ example/app.py
@@ -0,0 +1,25 @@
+import bottle
+import os
+import yaml
+from bottle_swagger import SwaggerPlugin
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+with open("{}/swagger.yml".format(this_dir)) as f:
+    swagger_def = yaml.load(f)
+
+bottle.install(SwaggerPlugin(swagger_def))
+
+
+@bottle.get('/thing')
+def hello():
+    return {"id": "1", "name": "Thing1"}
+
+
+@bottle.get('/thing/<thing_id>')
+def hello(thing_id):
+    return {"id": thing_id, "name": "Thing{}".format(thing_id)}
+
+
+@bottle.post('/thing')
+def hello():
+    return bottle.request.json
